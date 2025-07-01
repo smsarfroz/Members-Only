bfa083ef523b55b97e63e16d76f79473a1ada3e5
sarfroz <72331821+smsarfroz@users.noreply.github.com>
2025-07-01 07:25:44 +0000

    style the messages page

--- app.js
+++ app.js
@@ -135,7 +135,7 @@ app.post("/newmessage", async(req, res) => {
     const minutes = now.getMinutes().toString().padStart(2, '0');
     const seconds = now.getSeconds().toString().padStart(2, '0');
 
-    const timestamp = `${year}-${month}-${day} ${hours}:${minutes}:${seconds}`;
+    const timestamp = `${year}-${month}-${day} , ${hours}:${minutes}:${seconds}`;
 
     try {
       await db.addnewmessage(message, firstname, timestamp);
